124cd87a65d7d0a78016d61bf23a30446341204d
Roman Belov <gravirovshik@yandex.ru>
2024-09-21 04:19:43 +0300

    added files

---
+++ src/test/java/Homework4Tests.java
@@ -0,0 +1,37 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Homework4Tests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void selenideWikiTabTest() {
+        open("/selenide/selenide"); // открыли страницу
+        $("#wiki-tab").click(); // перешли на вики
+        $$(".internal.present").findBy(text("Soft assertions")).click(); // открыли страницу
+        $("#wiki-body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+                """));
+        sleep(6000);
+    }
+}
